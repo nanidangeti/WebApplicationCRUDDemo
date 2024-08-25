24f4715ec875389edce064e23d15a8c01c6a1a33
nanidangeti <nanid9611@gmail.com>
2024-08-25 13:49:31 +0530

    Add project files.

---
+++ WebApplicationCRUDDemo/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using WebApplicationCRUDDemo.Data;
+
+namespace WebApplicationCRUDDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        UserDbEntities userDbEntities = new UserDbEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetAllUsers()
+        {
+            var result =userDbEntities.UserTables.ToList();
+            return View(result);
+        }
+        [HttpGet]
+       
+        public ActionResult GetById(int? id)
+        {
+            var result = userDbEntities.UserTables.FirstOrDefault(u => u.Id == id);
+            return View(result);
+        }
+        [HttpGet]
+        public ActionResult AddNewUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddNewUser(int txtId,string txtName,string txtEmail,string txtPNum)
+        {
+            UserTable userTable = new UserTable();
+            userTable.Id = txtId;
+            userTable.UName = txtName;
+            userTable.Email = txtEmail;
+            userTable.PNum = txtPNum;
+
+            userDbEntities.UserTables.Add(userTable);
+            userDbEntities.SaveChanges();
+
+            return RedirectToAction("GetAllUsers");
+        }
+        [HttpGet]
+        public ActionResult EditRecord(int? id)
+        {
+            var Record = userDbEntities.UserTables.Find(id);
+            return View(Record);
+        }
+        [HttpPost]
+        public ActionResult EditRecord(int txtId,string txtName,string txtEmail,string txtPNum)
+        {
+            var recordTable = userDbEntities.UserTables.Find(txtId);
+            recordTable.Id = txtId;
+            recordTable.UName = txtName;
+            recordTable.Email = txtEmail;
+            recordTable.PNum = txtPNum;
+
+            
+            userDbEntities.SaveChanges();
+
+           
+             return RedirectToAction("GetAllUsers");
+
+        }
+        [HttpGet]
+        public ActionResult DeleteRecord(int? id)
+        
+        {
+            var Record = userDbEntities.UserTables.Find(id);
+            userDbEntities.UserTables.Remove(Record);
+            userDbEntities.SaveChanges();
+
+            return View(Record);
+
+           
+        }
+        [HttpPost]
+        public ActionResult DeleteNewRecord()
+        {
+
+           return RedirectToAction("GetAllUsers");
+        }
+
+    }
+}
---
+++ WebApplicationCRUDDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebApplicationCRUDDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplicationCRUDDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21ffa056-bf0c-428b-9683-192e7aebcdfb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
